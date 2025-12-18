b31d1910ece45408038f2a18616db5305b7a1f68
jiajie.zhang <16121081+jiajie_zhang@user.noreply.gitee.com>
2025-12-18 19:06:10 +0800

    # 增加debug模式

--- geometry_utils/src/selector.cpp
+++ geometry_utils/src/selector.cpp
@@ -118,7 +118,6 @@ std::vector<std::deque<GU::Point>> SegmentSelector::segmentChain(const std::vect
             GU::Point vec_c = normalized(grow2, grow);
             GU::Point vec_p = normalized(grow, pt);
             double cross1 = vec_c(0) * vec_p(1) - vec_c(1) * vec_p(0);
-            std::cout << "cross : " << cross1 << std::endl;
             // 线段匹配点与链的头/尾共线，删除头尾点
             if (std::fabs(cross1) < 1e-1) {
                 if (addToHead) {
